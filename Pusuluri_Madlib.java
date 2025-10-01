deea3bb8ba96834fe4acb361dc11c007a716b047
Tanush P <148394954+michealjordann@users.noreply.github.com>
2025-10-01 15:24:03 -0700

    Update and rename Madlib.java to Pusuluri_Madlib.java

---
+++ Pusuluri_Madlib.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pusuluri_Madlib
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        // Get user madlib
+        System.out.println("Type in a madlib, and use parenthesis when a singular/plural noun, verb, adjective, or number is needed. Ex: (noun)");
+        String sentence = sc.nextLine();
+
+        //user inputted adjs, verbs, etc
+            //use scanner yk how to do this bro 
+            
+        //Parsing algorithm
+            //take index of where user specifies replacement is needed using the string method for it i forgot and then use substring method to take where it is and replace method to replace with user inputted verb
+
+
+    }
+}
